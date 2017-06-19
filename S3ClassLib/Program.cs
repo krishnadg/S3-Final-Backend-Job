4fa841e55eea66235ca2eed80d3736c989d978e0
Ganesan <brf4@M-C02S80T6G8WM.nordstrom.net>
2017-06-19 11:51:13 -0700

    Project Setup and Initialization of Repo

---
+++ S3ClassLib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Amazon.S3;
+namespace S3ClassLib
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            
+        }
+    }
+}
---
+++ S3Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+namespace S3Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 4  );
+            
+        }
+        
+    }
+}
