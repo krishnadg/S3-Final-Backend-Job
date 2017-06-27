aad2577c79ec7a595a4df0b56dc2be1aa5dc9540
Ganesan <brf4@M-C02S80T6G8WM.nordstrom.net>
2017-06-27 11:08:18 -0700

    TNG, ORG, ORP and some tests setup

---
+++ S3JobFinal/S3ClassLib/Program.cs
@@ -0,0 +1,17 @@
+using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+
+ namespace S3ClassLib {
+     static class Program {
+
+         /// <summary>
+         /// The main entry point for the application.
+         /// </summary>
+         static void Main() {
+            
+         }
+     }
+
+ }
